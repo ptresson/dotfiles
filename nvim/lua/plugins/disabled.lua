6806956e62ccb003f05e4a670599fd2fd7195979
ptresson <paul.tresson@ird.fr>
2024-02-22 17:54:02 +0100

    replace file explorer with ranger

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "rcarriga/nvim-notify", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -13,4 +13,16 @@ return {
       })
     end,
   },
+  {
+    "kelly-lin/ranger.nvim",
+    config = function()
+      require("ranger-nvim").setup({ replace_netrw = true })
+      vim.api.nvim_set_keymap("n", "<leader>e", "", {
+        noremap = true,
+        callback = function()
+          require("ranger-nvim").open(true)
+        end,
+      })
+    end,
+  },
 }
