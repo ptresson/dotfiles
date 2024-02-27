70a7c4afa1013f2b8d7dd597e3a4933c203f6407
ptresson <paul.tresson@ird.fr>
2024-02-27 16:20:08 +0100

    disable inline hints for flash.nvim by default

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -25,4 +25,14 @@ return {
       })
     end,
   },
+  {
+    "folke/flash.nvim",
+    opts = {
+      modes = {
+        search = {
+          enabled = false,
+        },
+      },
+    },
+  },
 }
