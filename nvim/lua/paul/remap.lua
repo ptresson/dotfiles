5cbf167b039cc02c45cdefe8274df7533e9f87ab
ptresson <paul.tresson@ird.fr>
2024-02-29 18:35:37 +0100

    more quality of life rempas

--- nvim/lua/paul/remap.lua
+++ nvim/lua/paul/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<CR>", ":")
+vim.keymap.set("n","<S-CR>","o<Esc>k")
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<tab>", "<cmd>bn<CR>")
 vim.keymap.set("n", "<S-tab>", "<cmd>bp<CR>")
@@ -8,6 +9,8 @@ vim.keymap.set("n", "<leader>d", "<cmd>bd<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set('n','<leader>E', vim.diagnostic.goto_prev)
+vim.keymap.set('n','<leader>e', vim.diagnostic.goto_next)
 
 ---- greatest remap ever
 --vim.keymap.set("x", "<leader>p", [["_dP]])
