66a03fb49dab835b6b45dcf631f33af1b21a7041
ptresson <paul.tresson@ird.fr>
2024-03-01 16:08:11 +0100

    add nightfox just in case

--- nvim/lua/paul/lazy/color.lua
+++ nvim/lua/paul/lazy/color.lua
@@ -10,5 +10,15 @@ return{
             vim.cmd("colorscheme rose-pine")
         end
     },
+-- {
+--         "EdenEast/nightfox.nvim",
+--         config = function()
+--             require('nightfox').setup({
+--                 -- disable_background = true,
+--             })
+
+--             vim.cmd("colorscheme terafox")
+--         end
+--     },
 
 }
