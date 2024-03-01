c059b803b10f57e3b93f5a24a88f919174861d79
ptresson <paul.tresson@ird.fr>
2024-03-01 10:23:26 +0100

    keymap to config files

--- nvim/lua/paul/lazy/telescope.lua
+++ nvim/lua/paul/lazy/telescope.lua
@@ -8,11 +8,26 @@ return {
     },
 
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup({
+            defaults = {
+                vimgrep_arguments = {
+                    'rg',
+                    '--color=never',
+                    '--no-heading',
+                    '--with-filename',
+                    '--line-number',
+                    '--column',
+                    '--smart-case',
+                    '--hidden',
+                },
+            },
+        })
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
+        -- vim.keymap.set('n', '<leader>fc', builtin.find_files, {cwd = '~/.config/'})
+        vim.keymap.set('n', '<leader>fc', '<cmd>lua require("telescope.builtin").find_files({cwd = "~/.config/"})<CR>', {noremap = true, silent = true})
     end
 }
