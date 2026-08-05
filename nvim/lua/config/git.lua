722955b41a0de6adb46cb32da7aec0a86c016967
ptresson <paul.tresson@ird.fr>
2026-08-05 12:24:19 +0200

    add gitsigns

--- nvim/lua/config/git.lua
+++ nvim/lua/config/git.lua
@@ -1,2 +1,4 @@
 vim.pack.add({ 'https://github.com/kdheepak/lazygit.nvim' })
 vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>", { desc = "Open LazyGit" })
+
+vim.pack.add({'https://github.com/lewis6991/gitsigns.nvim'})
