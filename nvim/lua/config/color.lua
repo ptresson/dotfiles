c3ee3ad67c3f3f82089c356147f0ad520cf35734
ptresson <paul.tresson@ird.fr>
2026-08-05 18:13:48 +0200

    keep pywall in the radar

--- nvim/lua/config/color.lua
+++ nvim/lua/config/color.lua
@@ -1,3 +1,9 @@
 vim.pack.add({ 'https://github.com/rose-pine/neovim' })
+vim.pack.add({ {src='https://github.com/RedsXDD/neopywal.nvim', name='neopywal'} })
 require('rose-pine').setup({disable_background = true})
 vim.cmd("colorscheme rose-pine")
+
+
+-- require('neopywal').setup({transparent_background = true})
+-- -- require('neopywal').setup({})
+-- vim.cmd("colorscheme neopywal")
