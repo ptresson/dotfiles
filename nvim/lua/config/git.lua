31e9bad18550e0ed451e2491d1aac2a7e7d5d60f
ptresson <paul.tresson@ird.fr>
2026-08-14 10:38:15 +0200

    fix hunk navigation

--- nvim/lua/config/git.lua
+++ nvim/lua/config/git.lua
@@ -3,7 +3,22 @@ vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>", { desc = "Open LazyGit" })
 
 vim.pack.add({'https://github.com/lewis6991/gitsigns.nvim'})
 
-vim.keymap.set('n', '<leader>hs', require('gitsigns').stage_hunk)
-vim.keymap.set('n', '<leader>hr', require('gitsigns').reset_hunk)
-vim.keymap.set('n', '<leader>hn', require('gitsigns').nav_hunk('next'))
-vim.keymap.set('n', '<leader>hp', require('gitsigns').nav_hunk('prev'))
+local gitsigns = require('gitsigns')
+vim.keymap.set('n', '<leader>hs', gitsigns.stage_hunk)
+vim.keymap.set('n', '<leader>hr', gitsigns.reset_hunk)
+
+vim.keymap.set('n', '<leader>hn', function()
+  if vim.wo.diff then
+    vim.cmd.normal({']c', bang = true})
+  else
+    gitsigns.nav_hunk('next')
+  end
+end)
+
+vim.keymap.set('n', '<leader>hp', function()
+  if vim.wo.diff then
+    vim.cmd.normal({'[c', bang = true})
+  else
+    gitsigns.nav_hunk('prev')
+  end
+end)
