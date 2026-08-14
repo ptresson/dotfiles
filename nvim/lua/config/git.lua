6895992af13590c56db77dc39310a115c5a76c97
ptresson <paul.tresson@ird.fr>
2026-08-14 10:21:17 +0200

    hunk nav keymap

--- nvim/lua/config/git.lua
+++ nvim/lua/config/git.lua
@@ -5,3 +5,5 @@ vim.pack.add({'https://github.com/lewis6991/gitsigns.nvim'})
 
 vim.keymap.set('n', '<leader>hs', require('gitsigns').stage_hunk)
 vim.keymap.set('n', '<leader>hr', require('gitsigns').reset_hunk)
+vim.keymap.set('n', '<leader>hn', require('gitsigns').nav_hunk('next'))
+vim.keymap.set('n', '<leader>hp', require('gitsigns').nav_hunk('prev'))
